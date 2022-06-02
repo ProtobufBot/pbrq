7b8532d1e57deddc02ac294572099a435fc38455
lz1998 <875543533@qq.com>
2022-06-03 00:12:28 +0800

    update uri_reader

--- src/util/uri_reader.rs
+++ src/util/uri_reader.rs
@@ -8,15 +8,12 @@ pub async fn get_binary(uri: &str) -> RCResult<Vec<u8>> {
         return base64::decode(s).map_err(RCError::Base64Decode);
     }
     if let Some(file) = uri.strip_prefix("file://") {
-        let mut f = tokio::fs::File::open(file).await.map_err(RCError::IO)?;
-        let mut b = Vec::new();
-        f.read_buf(&mut b).await.map_err(RCError::IO)?;
-        return Ok(b);
+        return read_binary_file(file).await;
     }
     if uri.starts_with("https") || uri.starts_with("http") {
         return http_get(uri).await;
     }
-    Err(RCError::Other("protocol is not supported".into()))
+    read_binary_file(uri).await
 }
 
 pub async fn http_get(uri: &str) -> RCResult<Vec<u8>> {
@@ -33,4 +30,11 @@ pub async fn http_get(uri: &str) -> RCResult<Vec<u8>> {
         .await
         .map(|b| b.to_vec())
         .map_err(crate::error::RCError::Hyper)
+}
+
+pub async fn read_binary_file(path: &str) -> RCResult<Vec<u8>> {
+    let mut f = tokio::fs::File::open(path).await.map_err(RCError::IO)?;
+    let mut b = Vec::new();
+    f.read_buf(&mut b).await.map_err(RCError::IO)?;
+    return Ok(b);
 }
